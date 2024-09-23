740e125787b3c8e487b9fb2cca96d12af26f0f12
Amritha <amrithas172@gmail.com>
2024-09-23 21:12:45 +0530

    add frontend url

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,7 +9,7 @@ import path from "path";
 dotenv.config();
 connect();
 const app: Express = express();
-app.use(cors({origin: "https://image-manager-drab.vercel.app", credentials: true,}));
+app.use(cors({origin: process.env.FRONT_END_URL, credentials: true,}));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(morgan('dev'))
