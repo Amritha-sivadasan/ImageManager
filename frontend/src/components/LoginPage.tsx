c61dc4618b4a95abbcffacedb585a157dcbfe1ec
Amritha <amrithas172@gmail.com>
2024-10-02 15:39:36 +0530

    some change

--- frontend/src/components/LoginPage.tsx
+++ frontend/src/components/LoginPage.tsx
@@ -122,7 +122,7 @@ const LoginPage: React.FC = () => {
               type="submit"
               className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
             >
-              {loading ? <ClipLoader size={18} /> : "Sign In"}
+              {loading ? <ClipLoader size={20} /> : "Sign In"}
             </button>
           </div>
         </form>
