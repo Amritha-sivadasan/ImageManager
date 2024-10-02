1e258208aa62308fcb135214f59da1cd51c92fc7
Amritha <amrithas172@gmail.com>
2024-10-02 15:30:14 +0530

    add crone job

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,9 @@ import cors from "cors";
 import userRouter from "./router/userRoute";
 import  morgan from 'morgan'
 import path from "path";
+import cron from 'node-cron'
+import axios from "axios";
+
 
 dotenv.config();
 connect();
@@ -16,6 +19,22 @@ app.use(morgan('dev'))
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.use('/',userRouter)
+
+app.get('/ping', (req, res) => {
+  res.send('Server is alive');
+})
+
+cron.schedule('*/5 * * * *', async () => {
+  console.log('Cron job is running: Pinging the server...');
+
+  try {
+    // Ping your server to keep it alive
+    const response = await axios.get(`${process.env.BACKEND_URL || 'http://localhost:8000'}/ping`);
+    console.log('Ping successful:', response.status);
+  } catch (error:any) {
+    console.error('Error pinging the server:', error.message);
+  }
+});
 
 app.listen(8000, () => {
   console.log(`server is running on the ${8000}`);
